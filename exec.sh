b9d78e1c39186defb9db0eb93c669b0a8bcead74
Ker Tenepere <Ker10epere@gmail.com>
2024-11-01 17:23:42 +0800

    install jar file to m2 repository

--- exec.sh
+++ exec.sh
@@ -1,9 +1,24 @@
+# PACKAGE ONLY FOR TARGET DIRECTORY
 mvn -f provided-client/pom.xml clean package
-mvn -pl web-entrypoint/ clean package
+
+# PACKAGE FOR TARGET AND M2 REPOSITORY
+mvn -pl web-entrypoint/ clean install
 
 # ONLY INCLUDES BOOT-INF LIBRARIES
 java -jar web-entrypoint/target/web-entrypoint-0.0.1-SNAPSHOT.jar
 
 # ONLY INCLUDES SYSTEM JAR
 mvn -pl web-entrypoint -P dev spring-boot:run
+
+# DELETE FROM M2 REPOSITORY
+mvn -pl web-entrypoint/ dependency:purge-local-repository -DmanualInclude="com.begodly:web-entrypoint" -DsnapshotsOnly=true
+
+# INSTALL JAR TO M2 REPOSITORY
+mvn org.apache.maven.plugins:maven-install-plugin:3.1.3:install-file \
+                         -Dfile=provided-client/target/provided-client-0.0.1-SNAPSHOT.jar -DgroupId=com.begodly \
+                         -DartifactId=provided-client -Dversion=0.0.1-SNAPSHOT \
+                         -Dpackaging=jar 
+                         -DlocalRepositoryPath=<path>
+
+
 
